5b948c7752023c369a5bb13f4f7e1ecc2522c754
velobosv1977 <velobosv33@gmail.com>
2024-11-27 22:16:44 +0300

    Initial Commit HW9 completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //public static int[] generateRandomArray() {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        System.out.println("Homework 9. Array. Lesson 2");
+        System.out.println("");
+        //Task 1
+        System.out.println("Задача 1. Подсчёт затрат");
+        int[] arraySalary = {97876, 73645, 83478, 27987, 38726};
+        System.out.println(Arrays.toString(arraySalary));
+        int sumSalary = 0;
+        for (int sum : arraySalary) {
+            sumSalary += sum;
         }
+        System.out.println("Сумма трат за месяц составила " + sumSalary + " рублей");
+        System.out.println("");
+        //Task 2
+        System.out.println("Задача 2. Максимальные и минимальные затраты");
+        int minExp = arraySalary[0];
+        int maxExp = arraySalary[0];
+        for (int sum : arraySalary) {
+            if (sum < minExp) {
+                minExp = sum;
+            }
+            if (sum > maxExp) {
+                maxExp = sum;
+            }
+
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " +minExp+ " рублей. Максимальная сумма трат за неделю составила " +maxExp+ " рублей");
+        //Task 3
+        System.out.println("");
+        System.out.println("Задача 3. Среднее значение затрат");
+        // общая сумма затрат содержится в переменной sumSalary, среднее значение расчитаем из длины массива
+        double arrayAverage = (double) sumSalary / arraySalary.length;
+        System.out.println("Средняя сумма трат за месяц составила " + arrayAverage + " рублей");
+        //Task 4
+        System.out.println("");
+        System.out.println("Задача 4. Бухгалтерский баг");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.println((Arrays.toString(reverseFullName)));
+        for (int k = 0; k < reverseFullName.length / 2; k++) {
+            char temp = reverseFullName[k];
+            reverseFullName[k] = reverseFullName[reverseFullName.length - 1 - k];
+            reverseFullName[reverseFullName.length - 1 - k] = temp;
+        }
+        System.out.println(Arrays.toString(reverseFullName));
+
     }
+
 }
